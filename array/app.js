f942dd5ccabcd1edcd9171da2555877ea76efbff
Fridakhalo <aj050630@gmail.com>
2017-11-16 15:11:08 +0100

    Excercise 2

--- array/app.js
+++ array/app.js
@@ -1,1 +1,8 @@
-var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+
+	var array = ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 
+		'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre'];
+
+
+console.log(array);
+
+console.log(array[2]);
