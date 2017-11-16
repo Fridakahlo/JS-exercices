b3dcf830a31770f12a39a5fac1e9752a016f00aa
Fridakhalo <aj050630@gmail.com>
2017-11-16 15:59:07 +0100

    exercice 6 ana fred

--- array/app.js
+++ array/app.js
@@ -1,12 +1,13 @@
 
-	var array = ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 
-		'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre'];
+	var calendrier = new Array('Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 
+		'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Decembre', 'courgette');
 
+console.log(calendrier);
 
-console.log(array);
+console.log(calendrier[2]);
 
-console.log(array[2]);
+console.log(calendrier[4]);
 
-console.log(array[4]);
+console.log(calendrier[10]);
 
-console.log(array[10]);
+console.log(calendrier[7])
